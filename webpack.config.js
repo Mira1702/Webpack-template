141c1d4224e217b7077987a007fc309bcaa84313
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-12-02 21:54:09 +0200

    На стадии подключения CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const path = require ("path");
-
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
-    } 
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: "babel-loader"                          
-          }
-        ]
-    }   
-}
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [ 
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};
