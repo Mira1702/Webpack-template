e5a4604282f63e6e761db67c959d48d7c8f71bc2
Myroslava <67161084+Mira1702@users.noreply.github.com>
2020-12-03 00:21:11 +0200

    подключен CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
-const path = require ("path");
+const path = require("path");
+/* const ExtractTextPlugin = require('extract-text-webpack-plugin'); */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js"
-    } 
+    }, 
     module: {
         rules: [
           {
             test: /\.js$/,
             exclude: /node_modules/,
             use: "babel-loader"                          
-          }
-        ]
-    }   
+        },
+          {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }
+      ]
+  },
+    plugins: [ 
+      /* new ExtractTextPlugin({ filename: 'style.css' }) */
+      new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    })
+  ]
 }
